97e6eb0d7196e9367dac51b8c169049fd91b256d
Leslie Shea <leslieyshea@gmail.com>
2019-06-05 14:03:05 -0700

    event require fields test passing

--- test/models/Event.test.js
+++ test/models/Event.test.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Event = require('../../lib/models/Event');
 
 describe('Event model', () => {
-  it('has a name, description, date, time, and guild', () => {
+  it('has a name, description, date, time, and guild id', () => {
     const event = new Event({
       name: 'Raiding',
       description: 'Wanting to get folks together to run BFA raid',
@@ -19,5 +19,16 @@ describe('Event model', () => {
       guild: expect.any(mongoose.Types.ObjectId),
       _id: expect.any(mongoose.Types.ObjectId)
     });
+  });
+
+  it('requires a name, description, date, time, and guild id', () => {
+    const event = new Event({});
+
+    const errors = event.validateSync().errors;
+    expect(errors.name.message).toEqual('Path `name` is required.');
+    expect(errors.description.message).toEqual('Path `description` is required.');
+    expect(errors.date.message).toEqual('Path `date` is required.');
+    expect(errors.time.message).toEqual('Path `time` is required.');
+    expect(errors.guild.message).toEqual('Path `guild` is required.');
   });
 });
