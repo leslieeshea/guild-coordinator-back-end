3251cd59f225019f0844c235a45f152a75a25a0f
Leslie Shea <leslieyshea@gmail.com>
2019-06-06 13:45:31 -0700

    can create a new event route test passing

---
+++ test/routes/events.test.js
@@ -0,0 +1,28 @@
+const { getGuild, getAgent } = require('../data-helpers');
+
+describe('Event routes', () => {
+  it('can create an event', async() => {
+    const guild = await getGuild();
+    const id = guild._id;
+    const event = await getAgent()
+      .post('/api/v1/events')
+      .send({
+        title: 'Meeting tonight',
+        description: 'Five-man dungeons',
+        date: '06-06-2019',
+        time: '4:00 PM',
+        guild: id
+      });
+
+    expect(event.body).toEqual({
+      title: 'Meeting tonight',
+      description: 'Five-man dungeons',
+      date: '06-06-2019',
+      time: '4:00 PM',
+      guild: id,
+      _id: expect.any(String),
+      __v: 0
+      
+    });
+  });
+});
