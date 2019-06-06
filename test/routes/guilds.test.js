0fabfa8975310d6aef3302e2e37bbe8aeb8cdfd6
Leslie Shea <leslieyshea@gmail.com>
2019-06-06 11:32:08 -0700

    fixed can create a guild test

--- test/routes/guilds.test.js
+++ test/routes/guilds.test.js
@@ -1,24 +1,23 @@
-const request = require('supertest');
-const mongoose = require('mongoose');
-const app = require('../../lib/app');
+const { getUsers, getAgent } = require('../data-helpers');
 
 describe('Guilds routes', () => {
-  it('can create a guild', () => {
-    return request(app)
+  it('can create a guild', async() => {
+    const users = await getUsers();
+    const members = users.map(user => user._id);
+
+    const guild = await getAgent()
       .post('/api/v1/guilds')
       .send({
         name: 'Awesome Guild',
         game: 'World of Warcraft',
-        members: new mongoose.Types.ObjectId()
-      })
-      .then(res => {
-        expect(res.body).toEqual({
-          name: 'Awesome Guild',
-          game: 'World of Warcraft',
-          members: [expect.any(String)],
-          _id: expect.any(String),
-          __v: 0
-        });
+        members
       });
+    expect(guild.body).toEqual({
+      name: 'Awesome Guild',
+      game: 'World of Warcraft',
+      members,
+      _id: expect.any(String),
+      __v: 0
+    });
   });
 });
